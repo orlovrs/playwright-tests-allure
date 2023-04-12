a2c2868a2234008ffec5ed4b2df68d93dbcdde0b
Roman Orlov <helltester666@gmail.com>
2023-04-12 23:21:19 +0200

    another try

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,8 +5,6 @@ export default defineConfig({
   timeout: 30000,
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
   reporter: 'allure-playwright',
   use: {
     baseURL: 'https://orlovrs.github.io',
